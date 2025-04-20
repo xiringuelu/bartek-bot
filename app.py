26b46b33f7c51112f0945a51be969283116b67d7
xiringuelu <daniel.parrondo@gmail.com>
2025-04-20 12:23:16 +0100

    Other Fix

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def home():
 
 def run():
     #app.run(host='0.0.0.0', port=8080)
-    app.run(host='0.0.0.0', os.getenv('PORT'))
+    app.run(host='0.0.0.0', port=os.getenv('PORT'))
 
 def keep_alive():
     t = Thread(target=run)
